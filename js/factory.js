416869df6c5f4fcf04315fa4267e86a161669bda
Ben Cappello <bencappello@gmail.com>
2024-01-12 11:30:57 -0800

    Alter js file types

--- js/factory.js
+++ js/factory.js
@@ -1,8 +1,3 @@
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.13.2/jquery-ui.min.js" integrity="sha512-57oZ/vW8ANMjR/KQ6Be9v/+/h6bq9/l3f0Oc7vn6qMqyhvPd1cvKBRWWpzu0QoneImqr2SkmO4MSqU+RpHom3Q==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-<script src="https://cdn.jsdelivr.net/npm/timepicker@1.14.1/jquery.timepicker.min.js"></script>
-
-<script>
 //event details
 function dom(id) {
     return document.getElementById(id);
@@ -335,4 +330,3 @@ function setDisabledTimes() {
     endTime.timepicker('option', 'disableTimeRanges', []);
   }
 };
-</script>
